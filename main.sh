f91cddb8fc1ce84d81247e0319da05f034bfb454
rarchk <ronak.kogta@amagi.com>
2024-01-04 04:43:01 +0530

    few more debugging messages

--- main.sh
+++ main.sh
@@ -47,8 +47,8 @@ case "${ACTION}" in
 
         helm repo add serverless-chartmuseum "${ARTIFACTORY_URL}" --username "${ARTIFACTORY_USERNAME}" --password "${ARTIFACTORY_PASSWORD}"
         echo $UPSTREAM_BRANCH
-        UPSTREAM_CHART_VERSION=$(git show origin/"${UPSTREAM_BRANCH}":"${CHART_DIR}"/Chart.yaml | yq .version)
         echo "git show origin/${UPSTREAM_BRANCH}:${CHART_DIR}/Chart.yaml | yq .name"
+        UPSTREAM_CHART_VERSION=$(git show origin/"${UPSTREAM_BRANCH}":"${CHART_DIR}"/Chart.yaml | yq .version)
         UPSTREAM_CHART_NAME=$(git show origin/"${UPSTREAM_BRANCH}":"${CHART_DIR}"/Chart.yaml | yq .name)
         helm fetch serverless-chartmuseum/"${UPSTREAM_CHART_NAME}" --version "${UPSTREAM_CHART_VERSION}"
         helm template "${UPSTREAM_CHART_NAME}/${UPSTREAM_CHART_VERSION}.tgz" -f "${CHART_DIR}"/values.yaml > /tmp/upstream_values.yaml
