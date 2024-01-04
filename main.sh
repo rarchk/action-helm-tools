6da21964587e584a30fe4625fbc4888679ff26dd
rarchk <ronak.kogta@amagi.com>
2024-01-04 06:54:17 +0530

    debugging

--- main.sh
+++ main.sh
@@ -50,9 +50,8 @@ case "${ACTION}" in
         UPSTREAM_CHART_NAME=$(git show origin/"${UPSTREAM_BRANCH}":"${CHART_DIR}"/Chart.yaml | yq .name)
         #echo "helm fetch serverless-chartmuseum/${UPSTREAM_CHART_NAME} --version ${UPSTREAM_CHART_VERSION}"
         #helm pull "${UPSTREAM_CHART_NAME}" --version "${UPSTREAM_CHART_VERSION} --debug --repo "${ARTIFACTORY_URL}"  --username "${ARTIFACTORY_USERNAME}" --password "${ARTIFACTORY_PASSWORD}""
-        curl  --user "${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}" \ 
-        "${ARTIFACTORY_URL}/charts/${UPSTREAM_CHART_NAME}-${UPSTREAM_CHART_VERSION}.tgz" \ 
-        --output "${UPSTREAM_CHART_NAME}-${UPSTREAM_CHART_VERSION}.tgz"
+        echo curl  --user "${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}" "${ARTIFACTORY_URL}/charts/${UPSTREAM_CHART_NAME}-${UPSTREAM_CHART_VERSION}.tgz" --output "${UPSTREAM_CHART_NAME}-${UPSTREAM_CHART_VERSION}.tgz"
+        curl  --user "${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD}" "${ARTIFACTORY_URL}/charts/${UPSTREAM_CHART_NAME}-${UPSTREAM_CHART_VERSION}.tgz" --output "${UPSTREAM_CHART_NAME}-${UPSTREAM_CHART_VERSION}.tgz"
         ls
         helm template "${UPSTREAM_CHART_NAME}/${UPSTREAM_CHART_VERSION}.tgz" -f "${CHART_DIR}"/values.yaml > /tmp/upstream_values.yaml
 
