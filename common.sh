fd389d2c3461d3f97179cd8050f0949846a22280
rarchk <ronak.kogta@amagi.com>
2024-01-06 16:27:02 +0530

    debugging

--- common.sh
+++ common.sh
@@ -152,7 +152,6 @@ safe_exec(){
 
 send_github_comments() {
         COMMENT="#### $1 Output
-[Action Link](https://github.com/${{github.repository}}/commit/${{github.sha}}/checks/${{github.run_id}})
 <details>
 <summary>Details</summary>
 
