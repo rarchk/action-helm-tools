9dc3b806dbdd46da6a6c5be2f3e54ed416428098
Ronak Kogta <rarchk@users.noreply.github.com>
2024-03-05 23:35:33 +0530

    Update common.sh

--- common.sh
+++ common.sh
@@ -164,11 +164,9 @@ send_github_comments() {
         exit 0
     fi
     COMMENT="#### $1 Output
-<details>
-<summary>Details</summary>
-
+```diff
 $2
-</details>"
+```"
 
         PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
         COMMENTS_URL=$(cat "$GITHUB_EVENT_PATH" | jq -r .pull_request.comments_url)
