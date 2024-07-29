d370e4e3ce10ba83bdf5587889b74a56aa61b72f
Ronak Kogta <rarchk@users.noreply.github.com>
2024-07-29 06:18:00 +0530

    Update common.sh

--- common.sh
+++ common.sh
@@ -33,7 +33,7 @@ function helm_show(){
 
 dependency_repo_add(){
 pip3 install pyyaml --user
-python3 -c "import yaml;import os;from contextlib import suppress;f=open('$CHART_DIR/Chart.yaml','r');  p=yaml.safe_load(f.read());print('\n'.join('helm repo add ' + 'repo' + str(idx) + ' ' + i['repository'] for idx, i in enumerate(p['dependencies']))); f.close()" | xargs -I{} sh -c "{}" || true
+python3 -c "import yaml;import os;f=open('$CHART_DIR/Chart.yaml','r');  p=yaml.safe_load(f.read());print('\n'.join('helm repo add ' + 'repo' + str(idx) + ' ' + i['repository'] for idx, i in enumerate(p['dependencies']))) if 'dependencies' in p else print('Exception!'); f.close()" | xargs -I{} sh -c "{}" || true
 }
 get_chart_version(){
     if [ -n "$CHART_VERSION" ]; then
